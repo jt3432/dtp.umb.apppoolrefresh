9b2b713dbe0e3b3e57e0ac123574f14506f875c3
jt3432 <jason@ducttapeprogramming.com>
2015-08-11 13:56:43 -0500

    init

---
+++ dtp.umb.apppoolrefresh.plg/Controllers/AppPoolRefreshApiController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Configuration;
+using System.DirectoryServices;
+using System.Reflection;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Hosting;
+using System.Web.Http;
+using Umbraco.Core.Logging;
+using Umbraco.Web.Editors;
+using Umbraco.Web.Mvc;
+
+namespace dtp.umb.apppoolrefresh.plg.Controllers
+{
+    [PluginController("AppPoolRefresh")]
+    public class AppPoolRefreshApiController : UmbracoAuthorizedJsonController, IAppPoolRefreshApiController
+    {
+        #region Constants
+
+        private const string APP_SETTING_KEY = "AppPoolRefresh:TouchedOn";
+
+        #endregion
+
+        [HttpGet]
+        public bool Refresh()
+        {
+            bool success = false;
+            try
+            {
+                var configFile = WebConfigurationManager.OpenWebConfiguration("/");
+                var settings = configFile.AppSettings.Settings;
+                var currentDateTime = DateTime.Now.ToString();
+
+                if (settings[APP_SETTING_KEY] == null)
+                {
+                    settings.Add(APP_SETTING_KEY, currentDateTime);
+                }
+                else
+                {
+                    settings[APP_SETTING_KEY].Value = currentDateTime;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+                success = true;
+            }
+            catch (Exception ex)
+            {
+                // Opps
+                LogHelper.Error(typeof(AppPoolRefreshApiController), ex.Message, ex);
+            }
+            return success;
+        }
+    }
+}
---
+++ dtp.umb.apppoolrefresh.plg/Install/AddAppConfigKey.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using umbraco.interfaces;
+using System.Xml;
+using umbraco.cms.businesslogic.packager.standardPackageActions;
+using System.Configuration;
+using System.Web.Configuration;
+
+namespace dtp.umb.apppoolrefresh.plg.Install
+{
+
+    /// <summary>
+    /// Adds a key to the web.config app settings
+    /// </summary>
+    /// <remarks>Contribution from Paul Sterling</remarks>
+    public class AddAppConfigKey : IPackageAction
+    {
+        #region IPackageAction Members
+
+        public string Alias()
+        {
+            return "AddAppConfigKey";
+        }
+
+        public XmlNode SampleXml()
+        {
+            string sample = "<Action runat=\"install\" undo=\"true/false\" alias=\"AddAppConfigKey\" key=\"your key\" value=\"your value\"></Action>";
+            return helper.parseStringToXmlNode(sample);
+        }
+
+        public bool Execute(string packageName, XmlNode xmlData)
+        {
+            string addKey = string.Empty;
+            string addValue = string.Empty;
+
+            try
+            {
+                addKey = GetAttributeValueFromNode<string>(xmlData, "key", String.Empty);
+                addValue = GetAttributeValueFromNode<string>(xmlData, "value", String.Empty);
+
+                if (addKey != String.Empty)
+                {
+                    AddUpdateAppSettings(addKey, addValue, false);
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        public bool Undo(string packageName, XmlNode xmlData)
+        {
+            string addKey = string.Empty;
+
+            try
+            {
+                addKey = GetAttributeValueFromNode<string>(xmlData, "key", String.Empty);
+
+                if (addKey != string.Empty)
+                {
+                    RemoveAppSettings(addKey);
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region helpers
+
+        public T GetAttributeValueFromNode<T>(XmlNode node, string attributeName, T defaultValue)
+        {
+            if (node.Attributes[attributeName] != null)
+            {
+                string result = node.Attributes[attributeName].InnerText;
+                if (string.IsNullOrEmpty(result))
+                    return defaultValue;
+
+                return (T)Convert.ChangeType(result, typeof(T));
+            }
+            return defaultValue;
+        }
+
+        private bool RemoveAppSettings(string key)
+        {
+            bool success = false;
+            try
+            {
+                var configFile = WebConfigurationManager.OpenWebConfiguration("/");
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] != null)
+                {
+                    settings.Remove(key);
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+                success = true;
+            }
+            catch (Exception)
+            {
+                // Opps
+            }
+            return success;
+        }
+
+        private bool AddUpdateAppSettings(string key, string value, bool forceUpdate = true)
+        {
+            bool success = false;
+            try
+            {
+                var configFile = WebConfigurationManager.OpenWebConfiguration("/");
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else if (forceUpdate)
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+                success = true;
+            }
+            catch (Exception)
+            {
+                // Opps
+            }
+            return success;
+        }
+
+        #endregion
+    }
+}
